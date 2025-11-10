f4b68a05a1d412f38a613489e7e57487cf1d8f35
mehi raj <mehiraj8877@gmail.com>
2025-11-10 19:26:26 +0530

    Add basic calculator functionality

---
+++ . Simple Calculator .js
@@ -0,0 +1,13 @@
+// calculator.js
+function calculator(a, b, operator) {
+  switch (operator) {
+    case '+': return a + b;
+    case '-': return a - b;
+    case '*': return a * b;
+    case '/': return b !== 0 ? a / b : 'Cannot divide by zero';
+    default: return 'Invalid operator';
+  }
+}
+
+console.log(calculator(10, 5, '+'));
+console.log(calculator(10, 5, '/'));
