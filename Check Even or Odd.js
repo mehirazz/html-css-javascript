465af04a98cbcbfca53b8adf039e263a0c792562
mehi raj <mehiraj8877@gmail.com>
2025-11-10 19:26:47 +0530

    Add function to check if a number is even or odd

---
+++ Check Even or Odd.js
@@ -0,0 +1,11 @@
+// evenOdd.js
+function checkNumber(num) {
+  if (num % 2 === 0) {
+    console.log(`${num} is Even`);
+  } else {
+    console.log(`${num} is Odd`);
+  }
+}
+
+checkNumber(10);
+checkNumber(7);
